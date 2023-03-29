9b798439fe2b96d88bc76c39ad88075aefe840a4
Antoni Siekański <catus.pl@gmail.com>
2023-03-29 15:04:54 +0200

    Html layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,52 @@
-Simple, traffic-based pricing Sign-up for our 30-day trial. No credit card required. Pageviews $
-/month Monthly Billing Yearly Billing 25% discount Unlimited websites 100% data ownership Email
-reports Start my trial
+<header>
+	<h1>Simple, traffic-based pricing</h1>
+	<p>Sign-up for our 30-day trial. No credit card required.</p>
+</header>
+
+<main>
+	<p>100K Pageviews</p>
+
+	<input type="range" name="" id="" />
+
+	<p><span class="price">$16</span> /month</p>
+
+	<div class="switch">
+		<p>Monthly Billing</p>
+		<input type="checkbox" name="" id="" />
+		<p>Yearly Billing <span class="discount">25% discount</span></p>
+	</div>
+
+	<ul>
+		<li>Unlimited websites</li>
+		<li>100% data ownership</li>
+		<li>Email reports</li>
+	</ul>
+
+	<button>Start my trial</button>
+</main>
+
+<!-- ---  -->
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@600;800&display=swap');
+
+	:root {
+		/* Primary */
+		--clr-Soft-Cyan: hsl(174, 77%, 80%); /* (Full Slider Bar) */
+		--clr-Strong-Cyan: hsl(174, 86%, 45%); /* (Slider Backround) */
+		--clr-Light-Grayish-Red: hsl(14, 92%, 95%); /* (Discount Background) */
+		--clr-Light-Red: hsl(15, 100%, 70%); /* (Discount Text) */
+		--clr-Pale-Blue: hsl(226, 100%, 87%); /* (CTA Text) */
+		/* Neutral */
+		--clr-White: hsl (0, 0%, 100%); /* (Pricing Component Background) */
+		--clr-Very-Pale-Blue: hsl(230, 100%, 99%); /* (Main Background) */
+		--clr-Light-Grayish-Blue: hsl(224, 65%, 95%); /* (Empty Slider Bar) */
+		--clr-Light-Grayish-Blue: hsl(223, 50%, 87%); /* (Toggle Background) */
+		--clr-Grayish-Blue: hsl(225, 20%, 60%); /* (Text) */
+		--clr-Dark-Desaturated-Blue: hsl(227, 35%, 25%); /* (Text & CTA Background) */
+	}
+
+	:global(body) {
+		font-family: 'Manrope', sans-serif;
+		font-size: 15px;
+	}
+</style>
